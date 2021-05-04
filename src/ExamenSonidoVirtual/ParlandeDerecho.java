0f07bf53573a8c7192d9f779c1d933e684b13e44
Aaron <Aaron@Aarn420>
2021-05-03 18:22:55 -0600

    Examen Spottify

--- src/ExamenSonidoVirtual/ParlandeDerecho.java
+++ src/ExamenSonidoVirtual/ParlandeDerecho.java
@@ -14,11 +14,35 @@ import java.util.Scanner;
 public class ParlandeDerecho {
 
     public static void main(String[] args) {
-        String sonarderecho = "señal audio derecho: ";
-        System.out.println(sonarderecho + "1000110");
+        String sonarderecho = " ";
+        int volumenderecho = 0;
 
         Scanner audio = new Scanner(System.in);
+        
 
+        while (true) {
+            System.out.println("lista del ParlanteDerecho");
+            System.out.println("Sonar");
+            System.out.println("Volumen");
+            System.out.println("Habilar o Deshabilitar");
+            System.out.println("Lista de Reproduccion de Audios");
+            int nuny = audio.nextInt();
+
+            switch (nuny) {
+                case 1: 
+                    System.out.println("audio de musica derecho " + sonarderecho);
+                    break;
+                case 2: 
+                    System.out.println("nivel de volumen " + volumenderecho);
+                    break;
+                case 3:
+                    
+                    
+
+                    
+                
+            }
+
+        }
     }
-
 }
